976603d35cc341e883a928d02ffe89773c3ec2bf
fransvanderlek <fransvanderlek@gmail.com>
2021-04-09 12:29:43 +0200

    Update cmd.sh

--- docker-build/cmd.sh
+++ docker-build/cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/fransvanderlek/opcuaproxy.git
 cd opcuaproxy
-mvn clean dependency:copy-dependencies package
+./build.sh
 docker build . -t opcua-proxy:latest

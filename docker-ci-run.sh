f56229b2a938504b56c94cfe6264bc5942092972
fransvanderlek <fransvanderlek@gmail.com>
2021-04-10 19:51:20 +0200

    Update docker-ci-run.sh

--- docker-ci-run.sh
+++ docker-ci-run.sh
@@ -1,1 +1,1 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock -ti docker-ci
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v maven_repository_vol:/maven_repository -ti docker-ci

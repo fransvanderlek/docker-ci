079dd8d8f4536e298df012e7ae94a151cccf411c
fransvanderlek <fransvanderlek@gmail.com>
2021-04-11 18:49:08 +0200

    22

--- graalvm/docker-ci-aot-graalvm.sh
+++ graalvm/docker-ci-aot-graalvm.sh
@@ -1,1 +1,2 @@
-docker run --env GIT_REPO_URL=https://github.com/graalvm/graalvm-demos --env PROJECT_DIR=graalvm-demos/native-list-dir -v /var/run/docker.sock:/var/run/docker.sock -v maven_repository_vol:/root/.m2 -it graalvm-ci
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --env GIT_REPO_URL=https://github.com/graalvm/graalvm-demos.git --env PROJECT_DIR=graalvm-demos/native-list-dir -v /var/run/docker.sock:/var/run/docker.sock -v maven_repository_vol:/root/.m2 -it graalvm-ci

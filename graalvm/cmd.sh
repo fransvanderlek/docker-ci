b7572e7575d4f200c18d719eb41739c88ad05ba9
Frans <fransvanderlek@gmail.com>
2021-04-12 20:23:02 +0200

    changed

--- graalvm/cmd.sh
+++ graalvm/cmd.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-BRANCH_OPT = ""
-if [ -z "$BRANCH" ]
-then
-  echo $BRANCH_OPT
-else
-  $BRANCH_OPT = "--single-branch --branch $BRANCH"
-fi
-
-git clone $BRANCH_OPT $GIT_REPO_URL
+git clone $GIT_REPO_URL
 cd $PROJECT_DIR
 /bin/sh ./build.sh

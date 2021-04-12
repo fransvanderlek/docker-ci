95bd35eb24bb1e3ae4368251f392274bef9bdd6b
Frans <fransvanderlek@gmail.com>
2021-04-12 18:13:11 +0200

    added branching to ci

--- graalvm/cmd.sh
+++ graalvm/cmd.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-git clone $GIT_REPO_URL
+
+BRANCH_OPT = ""
+if [ -z "$BRANCH" ]
+then
+else
+  BRANCH_OPT = "--single-branch --branch $BRANCH"
+fi
+
+git clone $BRANCH_OPT $GIT_REPO_URL
 cd $PROJECT_DIR
 /bin/sh ./build.sh

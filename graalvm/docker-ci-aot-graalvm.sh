2b9e519ef6a993c02869f719a68b883cb193a7aa
Frans <fransvanderlek@gmail.com>
2021-04-12 18:16:10 +0200

    changed run ci

--- graalvm/docker-ci-aot-graalvm.sh
+++ graalvm/docker-ci-aot-graalvm.sh
@@ -1,2 +1,2 @@
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker run --env GIT_REPO_URL=https://github.com/fransvanderlek/graalvm-demos.git --env PROJECT_DIR=graalvm-demos/native-image-workshop -v /var/run/docker.sock:/var/run/docker.sock -v maven_repository_vol:/root/.m2 -it graalvm-ci
+docker run --env BRANCH=native-image --env GIT_REPO_URL=https://github.com/fransvanderlek/graalvm-demos.git --env PROJECT_DIR=graalvm-demos/native-image-workshop -v /var/run/docker.sock:/var/run/docker.sock -v maven_repository_vol:/root/.m2 -it graalvm-ci

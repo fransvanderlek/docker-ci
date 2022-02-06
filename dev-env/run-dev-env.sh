a0d99811eeb0ce5b72daa3aed66980d8ab6b3db5
fransvanderlek <fransvanderlek@gmail.com>
2022-02-06 18:19:59 +0100

    permissions

--- dev-env/run-dev-env.sh
+++ dev-env/run-dev-env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --init -p 3000:3000 -v "$(pwd):/home/workspace:cached" dev-env:latest
+docker run -it --init -p 3000:3000 -v "$(pwd):/home/workspace:cached" -v "maven_repository_vol:/maven_repository" dev-env:latest
